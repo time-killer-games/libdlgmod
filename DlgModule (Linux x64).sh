aaf84e2e6bfbf90ca301b88a5f0f1d789e9c32e2
time-killer-games <samuelvenable@hotmail.com>
2021-03-14 14:04:47 -0400

    Update DlgModule (Linux x64).sh

--- DlgModule (Linux x64).sh
+++ DlgModule (Linux x64).sh
@@ -2,4 +2,4 @@ cd "${0%/*}"
 
 mkdir "DlgModule (x64)"
 mkdir "DlgModule (x64)/Linux"
-g++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng.cpp" -o "DlgModule (x64)/Linux/libdlgmod.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lprocps -lpthread -fPIC -m64
+g++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng.cpp" -o "DlgModule (x64)/Linux/libdlgmod.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lpthread -fPIC -m64
