a8a146f0d7650ea93b4e299d4183da433e862f24
time-killer-games <samuelvenable@hotmail.com>
2021-03-25 11:54:39 -0400

    Add files via upload

--- DlgModule (Darwin x64).sh
+++ DlgModule (Darwin x64).sh
@@ -3,4 +3,4 @@ cd "${0%/*}"
 mkdir "DlgModule (x64)"
 mkdir "DlgModule (x64)/Darwin"
 export SDKROOT=`xcrun --show-sdk-path`
-/opt/local/bin/g++-mp-* "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x64)/Darwin/libdlgmod.dylib" -std=c++17 -shared  -static-libgcc -static-libstdc++ -I/opt/X11/include -L/opt/X11/lib -lX11 -fPIC -m64
+/opt/local/bin/g++-mp-* "DlgModule/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/xproc.cpp" "DlgModule/xlib/lodepng/lodepng.cpp" -o "DlgModule (x64)/Darwin/libdlgmod.dylib" -std=c++17 -shared  -static-libgcc -static-libstdc++ -I/opt/X11/include -L/opt/X11/lib -lX11 -fPIC -m64
