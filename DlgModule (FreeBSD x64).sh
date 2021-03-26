be215a427ac4eb2603d66f7b5139b7a68f189468
time-killer-games <samuelvenable@hotmail.com>
2021-03-26 15:16:46 -0400

    Add files via upload

--- DlgModule (FreeBSD x64).sh
+++ DlgModule (FreeBSD x64).sh
@@ -2,4 +2,4 @@ cd "${0%/*}"
 
 mkdir "DlgModule (x64)"
 mkdir "DlgModule (x64)/FreeBSD"
-clang++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/xproc.cpp" "DlgModule/xlib/lodepng.cpp" -o "DlgModule (x64)/FreeBSD/libdlgmod.so" -std=c++17 -shared -lX11 -lprocstat -lutil -lc -lpthread -fPIC -m64
+clang++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng.cpp" -o "DlgModule (x64)/FreeBSD/libdlgmod.so" -std=c++17 -shared -lX11 -lprocstat -lutil -lc -lpthread -fPIC -m64
--- DlgModule (Linux x86).sh
+++ DlgModule (Linux x86).sh
@@ -2,4 +2,4 @@ cd "${0%/*}"
 
 mkdir "DlgModule (x86)"
 mkdir "DlgModule (x86)/Linux"
-g++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/xproc.cpp" "DlgModule/xlib/lodepng.cpp" -o "DlgModule (x86)/Linux/libdlgmod.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lprocps -lpthread -fPIC -m32
+g++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/lodepng.cpp" -o "DlgModule (x86)/Linux/libdlgmod.so" -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lprocps -lpthread -fPIC -m32
