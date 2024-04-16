0abf90eb8b1b8b21e1af7d8860d1626e5f2398a4
Samuel Venable <samuelvenable@hotmail.com>
2024-04-16 02:23:30 -0400

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 cd "${0%/*}"
 
 if [ `uname` = "Darwin" ]; then
-  clang++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/MacOSX/dlgmodule.mm" -o "libdlgmod.dylib" -shared -std=c++17 -ObjC++ -framework AppKit -framework UniformTypeIdentifiers -mmacosx-version-min=10.13 -arch arm64 -arch x86_64 -fPIC
+  clang++ "libdlgmod/general/libdlgmod.cpp" "libdlgmod/macos/libdlgmod.mm" -o "libdlgmod.dylib" -I. -std=c++17 -shared -ObjC++ -framework AppKit -framework UniformTypeIdentifiers -mmacos-version-min=10.13 -arch arm64 -arch x86_64 -fPIC
 elif [ $(uname) = "Linux" ]; then
-  g++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/apiprocess/process.cpp" "DlgModule/xlib/xprocess.cpp" "DlgModule/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -IDlgModule/xlib/ -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lpthread -fPIC
+  g++ "libdlgmod/general/libdlgmod.cpp" "libdlgmod/xlib/libdlgmod.cpp" "libdlgmod/xlib/apiprocess/process.cpp" "libdlgmod/xlib/xprocess.cpp" "libdlgmod/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -I. -std=c++17 -shared -static-libgcc -static-libstdc++ -lX11 -lpthread -fPIC
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/apiprocess/process.cpp" "DlgModule/xlib/xprocess.cpp" "DlgModule/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -IDlgModule/xlib/ -std=c++17 -Wno-format-security -I/usr/local/include -L/usr/local/lib -shared -lX11 -lkvm -lc -lpthread -fPIC
+  clang++ "libdlgmod/general/libdlgmod.cpp" "libdlgmod/xlib/libdlgmod.cpp" "libdlgmod/xlib/apiprocess/process.cpp" "libdlgmod/xlib/xprocess.cpp" "libdlgmod/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -I. -std=c++17 -I/usr/local/include -L/usr/local/lib -shared -lX11 -lkvm -lc -lpthread -fPIC
 elif [ $(uname) = "DragonFly" ]; then
-  g++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/apiprocess/process.cpp" "DlgModule/xlib/xprocess.cpp" "DlgModule/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -IDlgModule/xlib/ -std=c++17 -I/usr/local/include -L/usr/local/lib -shared -static-libgcc -static-libstdc++ -lX11 -lkvm -lc -lpthread -fPIC
+  g++ "libdlgmod/general/libdlgmod.cpp" "libdlgmod/xlib/libdlgmod.cpp" "libdlgmod/xlib/apiprocess/process.cpp" "libdlgmod/xlib/xprocess.cpp" "libdlgmod/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -I. -std=c++17 -I/usr/local/include -L/usr/local/lib -shared -static-libgcc -static-libstdc++ -lX11 -lkvm -lc -lpthread -fPIC
 elif [ $(uname) = "NetBSD" ]; then
-  clang++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/apiprocess/process.cpp" "DlgModule/xlib/xprocess.cpp" "DlgModule/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -IDlgModule/xlib/ -std=c++17 -Wno-format-security -I/usr/local/include -L/usr/local/lib -shared -lX11 -lkvm -lc -lpthread -fPIC
+  g++ "libdlgmod/general/libdlgmod.cpp" "libdlgmod/xlib/libdlgmod.cpp" "libdlgmod/xlib/apiprocess/process.cpp" "libdlgmod/xlib/xprocess.cpp" "libdlgmod/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -I. -std=c++17 -I/usr/pkg/include -L/usr/pkg/lib -shared -static-libgcc -static-libstdc++ -lX11 -lkvm -lc -lpthread -fPIC
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/apiprocess/process.cpp" "DlgModule/xlib/xprocess.cpp" "DlgModule/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -IDlgModule/xlib/ -std=c++17 -Wno-format-security -I/usr/local/include -L/usr/local/lib -shared -lX11 -lkvm -lc -lpthread -fPIC
+  clang++ "libdlgmod/general/libdlgmod.cpp" "libdlgmod/xlib/libdlgmod.cpp" "libdlgmod/xlib/apiprocess/process.cpp" "libdlgmod/xlib/xprocess.cpp" "libdlgmod/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -I. -std=c++17 -I/usr/local/include -L/usr/local/lib -shared -lX11 -lkvm -lc -lpthread -fPIC
 elif [ $(uname) = "SunOS" ]; then
-  clang++ "DlgModule/Universal/dlgmodule.cpp" "DlgModule/xlib/dlgmodule.cpp" "DlgModule/xlib/apiprocess/process.cpp" "DlgModule/xlib/xprocess.cpp" "DlgModule/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -IDlgModule/xlib/ -std=c++17 -Wno-format-security -I/usr/local/include -L/usr/local/lib -shared -lX11 -lkvm -lc -lpthread -fPIC
+  g++ "libdlgmod/general/libdlgmod.cpp" "libdlgmod/xlib/libdlgmod.cpp" "libdlgmod/xlib/apiprocess/process.cpp" "libdlgmod/xlib/xprocess.cpp" "libdlgmod/xlib/lodepng.cpp" -o "libdlgmod.so" -DPROCESS_GUIWINDOW_IMPL -DNULLIFY_STDERR -I. -std=c++17 -I/usr/local/include -L/usr/local/lib -shared -static-libgcc -lX11 -lkvm -lc -lpthread -fPIC
 fi
